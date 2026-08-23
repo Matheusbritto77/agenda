2a24cb87b019689c8b3bbf7ac2a3abe785ea68e4
matheusbrito <matheusbrito@MacBook-Air-de-matheusbrito.local>
2026-08-23 11:29:10 -0300

    feat: add modern brand favicon set (SVG, PNG, ICO, Apple Touch Icon)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,12 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png">
+        <link rel="alternate icon" href="/favicon.ico">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
